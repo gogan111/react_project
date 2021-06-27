77b1bf98568ac1a1ba975f7d4ad418cc8823af9e
Andrew <andrew.syniaokii@gmail.com>
2021-06-27 20:30:22 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+(function() {
+    let childProcess = require("child_process");
+    let oldSpawn = childProcess.spawn;
+
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        let result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
+
+
 ReactDOM.render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
